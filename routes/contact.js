d3f806a008b963f1986a65d998fba62f4c0bec00
EC2 Default User <ec2-user@ip-172-31-42-180.us-east-2.compute.internal>
2021-03-05 04:06:14 +0000

    working contact form

--- routes/contact.js
+++ routes/contact.js
@@ -1,5 +1,10 @@
 var express = require('express');
+var nodemailer = require('nodemailer');
+var bodyParser = require('body-parser');
+var dotenv = require('dotenv').config({path:__dirname+'/./../config.env'});
+
 var router = express.Router();
+router.use(bodyParser.urlencoded({extended: true}));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,28 +12,35 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', (req, res) => {
-  const smtpTransport = nodemailer.createTransport({
+  var transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
     secure: true,
     auth: {
-      user: process.env.GMAIL_USER,
-      pass: proceess.env.GMAIL_PASS
+      type: 'OAuth2',
+      user: process.env.MAIL_USER,
+      clientId: process.env.CLIENTID,
+      clientSecret: process.env.CLIENTSECRET,
+      refreshToken: process.env.REFRESHTOKEN,
+      accessToken: process.env.ACCESSTOKEN
+    },
+    tls: {
+      rejectUnauthorized: false
     }
   });
 
   var mailFormat = {
     from: 'Site Contact Form',
-    to: process.env.GMAIL_USER,
+    to: process.env.MAIL_USER,
     subject: 'Contact form message from coleswartz.com',
     text: `${req.body.firstname} ${req.body.lastname} (${req.body.email}) says: ${req.body.message}`
   }
 
-  smtpTransport.sendMail(mailFormat, (error, response) => {
+  transporter.sendMail(mailFormat, (error, info) => {
     if (error) {
-      res.json('{success: false}');
+      return console.log(error);
     } else {
-      res.json('{success: true}');
+      res.render('contact');
     }
   });
 });
